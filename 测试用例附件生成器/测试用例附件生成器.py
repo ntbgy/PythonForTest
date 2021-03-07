ffe17abaa6d7ee88252b26b6cb954dac9ff3638f
ntbgy <ntbgy@outlook.com>
2021-03-07 12:28:02 +0800

    2021年3月7日

--- 测试用例附件生成器/测试用例附件生成器.py
+++ 测试用例附件生成器/测试用例附件生成器.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# 文件名称：测试用例附件生成器
-# 开发工具：PyCharm
-# 开发人员：ntbgy
-# 开发时间：2021/2/5 16:34
 import xlrd
 import os
 import shutil
@@ -27,18 +23,20 @@ def 文件名(相对目录):
 
 def 测试用例附件(文件相对地址):
     项目案例 = xlrd.open_workbook(文件相对地址)
+    新建目录('测试用例附件')
     sheet1 = 项目案例.sheet_by_index(0)
     第1行 = sheet1.row_values(0)
     第1列 = sheet1.col_values(0)
     for i in range(1, sheet1.nrows):
         row_temp = sheet1.row_values(i)
         document = Document()
-        for j in range(6):
+        for j in range(10):
             x = str(第1行[j]) + '：'
-            document.add_heading(x, level=2)
+            document.add_heading(x, level=3)
             y = str(row_temp[j])
             document.add_paragraph(y)
-        document.add_heading('执行截图：', level=2)
+            document.add_paragraph('')
+        document.add_heading('执行截图：', level=3)
         document.add_paragraph('')
         第1列[i] = 第1列[i].replace('/', '')
         document.save("测试用例附件/{}.docx".format(第1列[i]))
@@ -46,7 +44,6 @@ def 测试用例附件(文件相对地址):
 
 def 主函数():
     新建目录('测试用例')
-    新建目录('测试用例附件')
     文件名列表 = 文件名('测试用例')
     计数 = 1
     for 文件名字 in 文件名列表:
